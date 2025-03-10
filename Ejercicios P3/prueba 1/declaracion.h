1f586452c605949ccb8c526e4d5f88a71c085496
Jusfravs <144054309+Jusfravs@users.noreply.github.com>
2025-03-10 11:53:29 -0500

    declaracion.h

--- Ejercicios P3/prueba 1/declaracion.h
+++ Ejercicios P3/prueba 1/declaracion.h
@@ -3,15 +3,13 @@
 #include<iostream>
 using namespace std;
 void asignacion (string &frase1){
- cout<<"Ingrese la frase"<<endl; //salida
+ cout<<"Ingrese la frase"<<endl; 
  cin.ignore();
- getline(cin,frase1); //entrada
+ getline(cin,frase1); 
  string frase2;
  frase2= "fundamentos de progrmacion 2025";
  cout<<frase1<<endl;
  cout<<frase2<<endl;
 
 }
-
-
 #endif // DECLARACION_H_INCLUDED
